640c6a38af20c4dfed75c61825747ab883abff26
goveo <yuriybrusentsov@gmail.com>
2021-02-09 22:58:58 +0200

    Added project structure

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          href="https://reactjs.org"
+          target="_blank"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+
+export default App;
